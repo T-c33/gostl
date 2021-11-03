96a174cf8fff6d025cff42d50bd3c13d4800ff28
T-c33 <1206466033@qq.com>
2021-11-03 10:55:45 +0800

    update

---
+++ gitname.sh
@@ -0,0 +1,15 @@
+git filter-branch -f --env-filter '
+OLD_EMAIL="tangyechen@bytedance.com"
+CORRECT_NAME="T-c33"
+CORRECT_EMAIL="1206466033@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
